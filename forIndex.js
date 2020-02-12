cc60a33ede84f709ff788a49b6452b07f03636f9
Emilie Luna <emiliechildress@gmail.com>
2020-02-11 16:49:52 -0800

    create factorial algorithm

--- forIndex.js
+++ forIndex.js
@@ -5,5 +5,20 @@ function reverseString() {
     document.getElementById('results').innerHTML = str.split('').reverse().join(''); //split string then reverse the array and join
 }
 
+// Factoralize a Number
+function factoralizeNumber() {
+   var num = document.getElementById('userInput2').value;
+   var product; 
+   for (var product = 1; num > 0; num--) {
+       product *= num;
+   }
+    document.getElementById('results2').innerHTML = product;
+   }
+
+
+   
+  
+ 
+
 
 
